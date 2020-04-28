d9cae1d90626be00cb6848bf8c3dba862793dbd3
Diego Rivera <drivera_22@hotmail.com>
2020-04-27 18:14:09 -0600

    Add account stuff to theme

---
+++ themes/default/download/download_custom.php
@@ -0,0 +1,6 @@
+<?php if (!defined('FLUX_ROOT')) exit; ?>
+<h2>Download</h2>
+<p>You can download the client here:</p>
+<a href="/images/myw3schoolsimage.jpg" Mediafire>
+  <img src="https://image.jimcdn.com/app/cms/image/transf/none/path/s034603ad472cbb30/image/i76d32407d1ad3d03/version/1587612461/image.png" alt="Mediafire">
+</a>
